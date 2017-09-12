14b6017cdae788b406dd90d4288c6499b9ad19e9
John Runyon <runyon3@gmail.com>
2017-09-12 18:54:08 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,19 @@
-function basicTeenager(age) {
+function basicTeenager(age) {if (age >= 13) { return ("You are a teenager!")}}
 
+function teenager(age) {if (age >= 13 && age <= 19){ return "You are a teenager!"} else {
+  return ("You are not a teenager")
+}}
+
+function ageChecker(age) {if (age >= 13 && age <= 19) { return ("You are a teenager!")} else {
+  return ("You are a kid")
+}else {
+  return ("You are a grownup")
 }
-
-function teenager(age) {
-
-}
-
-function ageChecker(age) {
 
 }
 
 function ternaryTeenager(age) {
-
+return  (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
